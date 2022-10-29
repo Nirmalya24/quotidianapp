d806eaca86aab3dfe068e37193255df8008073b0
zwang4-code <zwang10312@gmail.com>
2022-10-28 19:48:35 -0700

    create widget main page branch

---
+++ app/src/pages/Widgets.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../partials/Header';
+import Footer from '../partials/Footer';
+
+
+function Widgets() {
+    return (
+        <div className="flex flex-col min-h-screen overflow-hidden">
+
+        <Header />
+        <h1>
+            "Hello"
+        </h1>
+
+        <Footer />
+        </div>
+
+
+
+    )
+
+}
+
+export default Widgets;
