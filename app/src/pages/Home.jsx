49762c8fb4ce8af72cd07589fcd0726eb2de5c2b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-10-29 18:33:20 -0700

    Update home content

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -7,29 +7,26 @@ import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Testimonials from '../partials/Testimonials';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
+// import { useGoogleOneTapLogin } from "react-google-one-tap-login";
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
-        <FeaturesBlocks />
-        <Testimonials />
-        <Newsletter />
-
+        {/* <FeaturesBlocks /> */}
+        {/* <Testimonials /> */}
+        {/* <Newsletter /> */}
       </main>
 
       {/*  Site footer */}
       <Footer />
-
     </div>
   );
 }
