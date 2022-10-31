435b5e3662fcfdf0f01d34f0511f4d6ffaf53e9b
zwang4-code <zwang10312@gmail.com>
2022-10-30 23:22:46 -0700

    removed duplicate daisyUI attribute and modified .gitignore

--- app/src/css/tailwind.config.js
+++ app/src/css/tailwind.config.js
@@ -138,15 +138,12 @@ module.exports = {
   ],
   daisyui: {  // daisyUI config (optional)
     styled: true,
-    themes: true,
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
     darkTheme: "dark",
-  },
-  daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
 };
