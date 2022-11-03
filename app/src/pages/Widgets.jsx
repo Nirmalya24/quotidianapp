63e03896e926528e6da81596968835d61ffb2d4e
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-03 14:37:24 -0700

    Fix drag and drop

--- app/src/pages/Widgets.jsx
+++ app/src/pages/Widgets.jsx
@@ -10,64 +10,70 @@ import { render } from 'react-dom'
 import {Container} from '../partials/Container'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { StatefulTargetBox as TargetBox } from "../partials/TargetBox";
 
 function Widgets() {
-    const [menubar, setMenubar] = useState(false)
-    const [themebar, setThemebar] = useState(false)
-    const [widgetbar, setWidgetbar] = useState(false)
+  const [menubar, setMenubar] = useState(false);
+  const [themebar, setThemebar] = useState(false);
+  const [widgetbar, setWidgetbar] = useState(false);
 
-    const openMenubar = () => {
-        console.log("Open menu bar")
-        setMenubar(true)
-        setThemebar(false)
-        setWidgetbar(false)
-    }
+  const openMenubar = () => {
+    console.log("Open menu bar");
+    setMenubar(true);
+    setThemebar(false);
+    setWidgetbar(false);
+  };
 
-    const closeMenubar = () => {
-        console.log("Close menu bar")
-        setMenubar(false)
-        setThemebar(false)
-        setWidgetbar(false)
-    }
+  const closeMenubar = () => {
+    console.log("Close menu bar");
+    setMenubar(false);
+    setThemebar(false);
+    setWidgetbar(false);
+  };
 
-    const openThemebar = () => {
-        console.log("Open theme bar")
-        setThemebar(true)
-        setMenubar(false)
-        setWidgetbar(false)
-    }
+  const openThemebar = () => {
+    console.log("Open theme bar");
+    setThemebar(true);
+    setMenubar(false);
+    setWidgetbar(false);
+  };
 
-    const openWidgetbar = () => {
-        console.log("Open widget bar")
-        setWidgetbar(true)
-        setMenubar(false)
-        setThemebar(false)
-    }
+  const openWidgetbar = () => {
+    console.log("Open widget bar");
+    setWidgetbar(true);
+    setMenubar(false);
+    setThemebar(false);
+  };
 
-    return (
-        <div>
-            {/* <Background /> */}
-            {/* <DragDrop /> */}
-            <DndProvider backend={HTML5Backend}>
-                <Container widgetBarState={widgetbar}/>
-            </DndProvider>
-            <WidgetMenuBar
-                menubarState={menubar}
-                themebarState={themebar}
-                widgetbarState={widgetbar}
-                openThemebar={openThemebar}
-                openWidgetbar={openWidgetbar} />
-            <ThemeBar
-                themebarState={themebar}
-                closeMenubar={closeMenubar}
-                openMenubar={openMenubar} />
-            <WidgetBar
-                widgetbarState={widgetbar}
-                closeMenubar={closeMenubar}
-                openMenubar={openMenubar} />
+  return (
+    <div>
+      <Background />
+      {/* <DragDrop /> */}
+      <DndProvider backend={HTML5Backend}>
+        <div style={{ float: "left", marginLeft: "5rem", marginTop: ".5rem" }}>
+          <TargetBox />
         </div>
-    )
 
+        <WidgetMenuBar
+          menubarState={menubar}
+          themebarState={themebar}
+          widgetbarState={widgetbar}
+          openThemebar={openThemebar}
+          openWidgetbar={openWidgetbar}
+        />
+        <ThemeBar
+          themebarState={themebar}
+          closeMenubar={closeMenubar}
+          openMenubar={openMenubar}
+        />
+        <WidgetBar
+          widgetbarState={widgetbar}
+          closeMenubar={closeMenubar}
+          openMenubar={openMenubar}
+        />
+      </DndProvider>
+    </div>
+  );
 }
 
 export default Widgets;
--- app/src/partials/WidgetBar.jsx
+++ app/src/partials/WidgetBar.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import '../css/widget-page/ddcss.css';
+import { Container } from "./Container";
 
 function WidgetBar({ widgetbarState, closeMenubar, openMenubar }) {
-
-    return (
-        <div>
-            <div className={widgetbarState ? 'widgetbar-open glass' : 'widgetbar-closed'}>
-                <div className='bar-title'>
-                    <div onClick={openMenubar}>
-                        <i className='ri-arrow-left-circle-line'></i>
-                    </div>
-                    <div>Choose Widgets</div>
-                    <div onClick={closeMenubar} >
-                        <i className='ri-close-circle-line'></i>
-                    </div>
-                </div>
-                <div>
-                    <ul className="menu p-4 overflow-y-auto w-80">
-                    </ul>
-                </div>
-            </div >
+  return (
+    <div>
+      <div
+        className={widgetbarState ? "widgetbar-open glass" : "widgetbar-closed"}
+      >
+        <div className="bar-title">
+          <div onClick={openMenubar}>
+            <i className="ri-arrow-left-circle-line"></i>
+          </div>
+          <div>Choose Widgets</div>
+          <div onClick={closeMenubar}>
+            <i className="ri-close-circle-line"></i>
+          </div>
         </div>
-    )
+
+        {/* Widgets to be dragged and dropped container */}
+        <Container />
+      </div>
+    </div>
+  );
 }
 
 export default WidgetBar;
