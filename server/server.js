f03de8dba82f264c5771291f621bdc643cc5b168
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-04 20:30:18 -0700

    Bug fix

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const app = express();
 dotenv.config();
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
-app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 
 const users = [];
 
@@ -34,7 +34,7 @@ app.post("/test", (req, res) => {
 
 app.post("/api/google-login", async (req, res) => {
   console.log("Received request:", req.body);
-  const { token } = req.body;
+  const { token } = req.body.data;
   const ticket = await client.verifyIdToken({
     idToken: token,
     audience: process.env.CLIENT_ID,
