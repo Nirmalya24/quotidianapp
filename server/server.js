47f55e522fcdd6c9d96ce387481c76ba961b25c3
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-05 21:53:55 -0700

    Add prisma and MongoDB related queries

--- server/server.js
+++ server/server.js
@@ -24,6 +24,45 @@ function upsert(array, item) {
   }
 }
 
+const findUserByEmail = async (email) =>
+  await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+const createUser = async (user) => {
+  await prisma.user.create({
+    data: {
+      email: user.email,
+      first_name: user.given_name,
+      last_name: user.family_name,
+      google_id: user.sub,
+      token: user.token,
+    },
+  });
+};
+
+const updateUserToken = async (user) => {
+  await prisma.user.update({
+    where: {
+      email: user.email,
+    },
+    data: {
+      token: user.token,
+    },
+  });
+};
+
+const verifyToken = async (token) => {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  return payload;
+};
+
 app.get("/", (req, res) => {
   res.send("Quotidian server is running!");
 });
@@ -33,17 +72,33 @@ app.post("/test", (req, res) => {
   res.send("test");
 });
 
+/*
+  * Google One Tap Login
+  * https://developers.google.com/identity/gsi/web/reference/js-reference#googleonetap
+  * This function is responsible for verifying the token sent from the client
+  * and returning the user's data.
+  * If the user is not in the database, it will create a new user.
+  * If the user is in the database, it will update the user's token.
+  * The token is used to verify the user's identity when they make requests to the API.
+  * The token is stored in the database and sent to the client.
+  * 
+*/
 app.post("/api/google-login", async (req, res) => {
   console.log("Received request:", req.body);
-  const { token } = req.body;
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.CLIENT_ID,
-  });
-
-  console.log(ticket.getPayload());
-  const { name, email, picture } = ticket.getPayload();
-  upsert(users, { name, email, picture });
+  console.log("Checking if user is already in database...");
+  const user = findUserByEmail(req.body.email);
+  if (user) {
+    console.log("User already exists in database!");
+    console.log("Updating user token...");
+    await updateUserToken(req.body);
+    console.log("User token updated!");
+  } else {
+    console.log("User does not exist in database, creating user...");
+    createUser(req.body);
+  }
+  console.log("Verifying token...");
+  const payload = await verifyToken(req.body.token);
+  const { name, email, picture } = payload;
   res.header("Access-Control-Allow-Origin", "*");
   res.status(201);
   res.json({ name, email, picture });
