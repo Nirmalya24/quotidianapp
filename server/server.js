46ca72dcfea9bea3feda4c1c5da1af040a54ada6
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-05 22:37:37 -0700

    Bug fix

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 const findUserByEmail = async (email) => {
-  await prisma.user.findUnique({
+  await prisma.user.findOne({
     where: {
       email: email,
     },
