4b60fcedd0d910d792bd2e5e68dd85bcefb0cb7c
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-05 21:51:50 -0700

    Add test page route

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -14,6 +14,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
+import Test from "./pages/Test";
 
 
 function App() {
@@ -41,7 +42,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        {/* <Route path="/test" element={<Test />} /> */}
+        <Route path="/test" element={<Test />} />
       </Routes>
     </>
   );
