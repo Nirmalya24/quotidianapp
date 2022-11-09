b85d9f0b9c27893ad6299e2f94960fa3d0d404e9
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-09 13:31:44 -0800

    Updated Dashboard page

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Logo from "../images/Q..png";
 import DashboardSidebar from "../partials/DashboardSidebar";
 import DashboardMain from "../partials/DashboardMain";
+import AppDrawer from "../utils/AppDrawer";
+import { Outlet } from "react-router-dom";
 
 function Dashboard() {
   return (
+    // container
     <div className="h-screen w-screen flex bg-gray-200">
-      {/*  container */}
-
+      {/* Side navbar */}
       <DashboardSidebar />
-      <DashboardMain />
+      <Outlet />
     </div>
   );
 }
