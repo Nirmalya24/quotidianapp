e793007c7acdcdf2994a6b5f4cf11c6c4b58ba37
zwang4-code <zwang10312@gmail.com>
2022-11-09 03:06:35 -0800

    Add Timer to List of Widgets

--- app/src/partials/Widgets.jsx
+++ app/src/partials/Widgets.jsx
@@ -3,22 +3,31 @@ export const WIDGET_TYPE = 'widget'
 
 export const widgets = [{
   id: '1',
+  color: "lightgray",
+  url: 'TimerImage',
+  component: "Timer"
+}, {
+  id: '2',
   color: "yellow",
   url:
     'https://i5.walmartimages.com/asr/c9ac18ff-a698-45c3-a40e-6eacdd9077a8.e7b8c131f9254a2863bb9512a4611e4c.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',
+  component: ""
 }, {
-  id: '2',
+  id: '3',
   color: "blue",
   url:
     'https://i.etsystatic.com/16421349/r/il/7c67ce/3027303927/il_1588xN.3027303927_hx5b.jpg',
+    component: ""
 }, {
-  id: '3',
+  id: '4',
   color: "red",
   url:
     'https://i.etsystatic.com/16421349/r/il/d32f34/3927649281/il_1588xN.3927649281_it40.jpg',
+    component: ""
 }, {
-  id: '4',
+  id: '5',
   color: "green",
   url:
-  'https://i5.walmartimages.com/asr/b85ca1f5-7827-4057-ad41-d38a258355ff.9f47d77efed2336fadf99166218c2bd8.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',
+    'https://i5.walmartimages.com/asr/b85ca1f5-7827-4057-ad41-d38a258355ff.9f47d77efed2336fadf99166218c2bd8.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',
+    component: ""
 }];
