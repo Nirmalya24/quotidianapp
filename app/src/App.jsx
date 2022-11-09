3f581cb34094cb97343e4bdd4314522d5664ff8f
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-09 13:31:09 -0800

    Added various dashboard routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -15,6 +15,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 import Dashboard from "./pages/Dashboard";
+import DashboardSidebarNew from "./partials/DashboardSidebar";
+import DashboardMain from "./partials/DashboardMain";
+import NewsDashboard from "./partials/NewsDashboard";
+import ProductivityDashboard from "./partials/ProductivityDashboard";
+import ScheduleDashboard from "./partials/ScheduleDashboard";
+import Settings from "./partials/Settings";
 
 function App() {
   const location = useLocation();
@@ -41,8 +47,14 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/test" element={<Test />} /> */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="" element={<DashboardMain />} />
+          <Route path="news" element={<NewsDashboard />} />
+          <Route path="productivity" element={<ProductivityDashboard />} />
+          <Route path="schedule" element={<ScheduleDashboard />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="/test" element={<DashboardSidebarNew />} />
       </Routes>
     </>
   );
