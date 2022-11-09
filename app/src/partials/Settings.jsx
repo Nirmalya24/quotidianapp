cd607e1cfc2b09157298f013163aa3693e0714da
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-09 13:34:25 -0800

    Placeholder components for the various pages of the dashboard

---
+++ app/src/partials/Settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Settings() {
+  return (
+    <div className="flex">
+      <div className="h-screen flex-1 p-7">
+        <h1 className="text-2xl font-semibold">Settings</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
