8b477fabe85bff85fd1e98a729da32cab4926624
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-10 12:07:49 -0800

    Fix widget drawer click

--- app/src/partials/Dashboard/DashboardSidebar.jsx
+++ app/src/partials/Dashboard/DashboardSidebar.jsx
@@ -35,11 +35,7 @@ function DashboardSidebar() {
       src: <Emoji symbol="🌤" label="Weather" />,
       link: "/dashboard/weather",
     },
-    {
-      title: "Setting",
-      src: <Emoji symbol="⚙️" label="Settings" />,
-      link: "/dashboard/settings",
-    },
+
     {
       title: "Widget Drawer",
       src: <Emoji symbol="🎁" label="Drawer" />,
@@ -60,6 +56,12 @@ function DashboardSidebar() {
         },
       ],
     },
+    {
+      title: "Settings",
+      src: <Emoji symbol="⚙️" label="Settings" />,
+      link: "/dashboard/settings",
+      gap: true,
+    },
   ];
 
   const openMenuCloseDrawer = () => {
@@ -75,6 +77,10 @@ function DashboardSidebar() {
       setSubmenuOpen(!submenuOpen);
       e.preventDefault();
     }
+  };
+
+  const widgetClick = (widgetTitle) => {
+    console.log("Widget clicked: ", widgetTitle);
   };
 
   return (
@@ -105,37 +111,40 @@ function DashboardSidebar() {
             Quotidian.app
           </h1>
         </div>
-        <ul className="pt-6">
+        <ul className="pt-6 flex flex-col">
           {Menus.map((Menu, index) => (
-            <Link
-              to={Menu.link}
-              relative="path"
-              key={index}
-              onClick={(e) => openWidgetDrawer(e, Menu)}
-            >
-              <li
-                key={index}
-                className={`flex  rounded-md p-2 cursor-pointer hover:bg-[#FEA303] hover:text-white hover:font-bold font-semibold text-slate-300 text-md items-center gap-x-4 
-              ${Menu.gap ? "mt-8" : "mt-2"} ${
-                  index === 0 && "bg-light-white"
-                } `}
+            <div key={index}>
+              <Link
+                to={Menu.link}
+                relative="path"
+                onClick={(e) => openWidgetDrawer(e, Menu)}
               >
-                {Menu.src}
-                <span
-                  className={`${!open && "hidden"} origin-left duration-200`}
+                <li
+                  className={`flex  rounded-md p-2 cursor-pointer hover:bg-[#FEA303] hover:text-white hover:font-bold font-semibold text-slate-300 text-md items-center gap-x-4
+              ${Menu.gap ? "mt-20" : "mt-2"} ${
+                    index === 0 && "bg-light-white"
+                  } `}
                 >
-                  {Menu.title}
-                </span>
-                {Menu.submenu && open && (
-                  <BsChevronDown className={`${submenuOpen && "rotate-180"}`} />
-                )}
-              </li>
+                  {Menu.src}
+                  <span
+                    className={`${!open && "hidden"} origin-left duration-200`}
+                  >
+                    {Menu.title}
+                  </span>
+                  {Menu.submenu && open && (
+                    <BsChevronDown
+                      className={`${submenuOpen && "rotate-180"}`}
+                    />
+                  )}
+                </li>
+              </Link>
               {Menu.submenu && submenuOpen && open && (
                 <div className="grid grid-cols-2 pl-2">
                   {Menu.submenuItems.map((submenuItem, index) => (
                     <div
                       key={index}
                       className={`rounded-md cursor-pointer hover:bg-[#FEA303] hover:text-white hover:font-bold font-semibold text-slate-300 items-center`}
+                      onClick={() => widgetClick(submenuItem.title)}
                     >
                       <div className="card items-center text-center width-auto card-compact py-3">
                         <h2 className="text-xs">{submenuItem.title}</h2>
@@ -145,7 +154,7 @@ function DashboardSidebar() {
                   ))}
                 </div>
               )}
-            </Link>
+            </div>
           ))}
         </ul>
       </div>
