4c4e01964e5700f7b2606ea64e2e3af12e0aed1f
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-16 13:32:25 -0800

    Default Edges and Nodes state

---
+++ app/src/partials/Dashboard/Mindmap/nodes.ts
@@ -0,0 +1,10 @@
+import { Node } from 'reactflow';
+
+export default [
+    {
+        id: '0',
+        type: 'text',
+        data: { nodeText: '' },
+        position: { x: 250, y: 25 },
+    },
+] as Node[];
