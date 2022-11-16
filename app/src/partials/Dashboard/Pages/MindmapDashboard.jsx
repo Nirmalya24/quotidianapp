e00094d11e823aac19d1a266c858ce810c33024a
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-16 13:35:44 -0800

    Mindmap dashboard page

---
+++ app/src/partials/Dashboard/Pages/MindmapDashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MindmapWidget from "../Widgets/MindmapWIdget";
+
+function MindmapDashboard() {
+  return (
+    <div className="flex">
+      <div className="h-screen flex-1 p-7">
+        <h1 className="text-2xl font-semibold">Mindmap dashboard</h1>
+        <MindmapWidget />
+      </div>
+    </div>
+  );
+}
+
+export default MindmapDashboard;
