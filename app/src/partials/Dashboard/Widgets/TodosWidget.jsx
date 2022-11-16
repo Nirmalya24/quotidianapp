b6a2b2eceb2f5a8cdcc34752b0b1f057bf96f2ba
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-16 14:38:38 -0800

    Save Todos in LocalStorage to persist them

--- app/src/partials/Dashboard/Widgets/TodosWidget.jsx
+++ app/src/partials/Dashboard/Widgets/TodosWidget.jsx
@@ -1,83 +1,80 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 
 function TodosWidget() {
-  const [currentTodo, setCurrentTodo] = useState("");
-  const [todos, setTodos] = useState([
-    {
-      todo: "make a todo widget",
-      isCompleted: true,
-    },
-    {
-      todo: "style the todo widget",
-      isCompleted: true,
-    },
-    {
-      todo: "put it on dashboard",
-      isCompleted: true,
-    },
-  ]);
+    const [currentTodo, setCurrentTodo] = useState("");
 
-  function createNewTodo(currentTodo) {
-    let todosArray = [...todos];
-    todosArray.push({
-      todo: currentTodo,
-      isCompleted: false,
-    });
-    setTodos(todosArray);
-  }
+    const [todos, setTodos] = useState(
+        localStorage.getItem("todos")
+            ? JSON.parse(localStorage.getItem("todos"))
+            : []
+    );
 
-  function completeTodo(index) {
-    let todosArray = [...todos];
-    todosArray[index].isCompleted = !todosArray[index].isCompleted;
-    setTodos(todosArray);
-  }
+    // useEffect to update the todos in localStorage
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
-  function deleteTodo(index) {
-    let todosArray = [...todos];
-    todosArray.splice(index, 1);
-    setTodos(todosArray);
-  }
+    function createNewTodo(currentTodo) {
+        let todosArray = [...todos];
+        todosArray.push({
+            todo: currentTodo,
+            isCompleted: false,
+        });
+        setTodos(todosArray);
+    }
 
-  return (
-    <div className="form-control w-full max-w-xs">
-      <label className="label">
-        <span className="label-text text-white">What needs to get done?</span>
-      </label>
-      <input
-        type="text"
-        placeholder="Type your todo here"
-        className="input input-bordered w-full max-w-xs my-2 text-white"
-        value={currentTodo}
-        onChange={(e) => setCurrentTodo(e.target.value)}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            createNewTodo(currentTodo);
-            setCurrentTodo("");
-          }
-        }}
-      ></input>
+    function completeTodo(index) {
+        let todosArray = [...todos];
+        todosArray[index].isCompleted = !todosArray[index].isCompleted;
+        setTodos(todosArray);
+    }
 
-      {todos.map((todo, index) => (
-        <div key={index} className="flex items-center py-1">
-          <input
-            type="checkbox"
-            className="checkbox-success mx-2 cursor-pointer"
-            checked={todo.isCompleted}
-            onClick={() => completeTodo(index)}
-            onChange={() => {
-              todo.isCompleted;
-            }}
-          />
-          <div className={`grow ${todo.isCompleted ? "line-through" : ""}`}>
-            {todo.todo}
-          </div>
-          <div onClick={() => deleteTodo(index)} className="cursor-pointer">
-            &#128465;
-          </div>
+    function deleteTodo(index) {
+        let todosArray = [...todos];
+        todosArray.splice(index, 1);
+        setTodos(todosArray);
+    }
+
+    return (
+        <div className="form-control w-full max-w-xs">
+            <label className="label">
+                <span className="label-text text-white">What needs to get done?</span>
+            </label>
+            <input
+                type="text"
+                placeholder="Type your todo here"
+                className="input input-bordered w-full max-w-xs my-2 text-white"
+                value={currentTodo}
+                onChange={(e) => setCurrentTodo(e.target.value)}
+                // On Enter key down, create a new todoItem
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        createNewTodo(currentTodo);
+                        setCurrentTodo("");
+                    }
+                }}
+            ></input>
+
+            {todos.map((todo, index) => (
+                <div key={index} className="flex items-center py-1">
+                    <input
+                        type="checkbox"
+                        className="checkbox-success mx-2 cursor-pointer"
+                        checked={todo.isCompleted}
+                        onClick={() => completeTodo(index)}
+
+                    />
+                    <div className={`grow ${todo.isCompleted ? "line-through" : ""}`}>
+                        {todo.todo}
+                    </div>
+                    <div onClick={() => deleteTodo(index)} className="cursor-pointer">
+                        &#128465;
+                    </div>
+                </div>
+            ))}
+            {todos.length > 0 && `${todos.length} items`}
         </div>
-      ))}
-      {todos.length > 0 && `${todos.length} items`}
-    </div>
-  );
+    );
 }
+
 export default TodosWidget;
