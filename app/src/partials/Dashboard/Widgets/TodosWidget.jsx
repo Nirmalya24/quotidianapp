00011ef096bd129ec9f51425c3a3ba53332ebfcf
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-17 21:07:48 -0800

    Added email check to retrive todos

--- app/src/partials/Dashboard/Widgets/TodosWidget.jsx
+++ app/src/partials/Dashboard/Widgets/TodosWidget.jsx
@@ -6,7 +6,9 @@ function TodosWidget() {
   //   const URL = "https://quotidianapp-dev.up.railway.app/api";
   const URL = "http://localhost:5001/api";
   const [email, setEmail] = useState(
-    JSON.parse(localStorage.getItem("loginData")).email
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData")).email
+      : null
   );
   const [currentTodo, setCurrentTodo] = useState("");
 
@@ -91,23 +93,29 @@ function TodosWidget() {
 
   return (
     <div className="form-control w-full max-w-xs">
-      <label className="label">
-        <span className="label-text text-white">What needs to get done?</span>
-      </label>
-      <input
-        type="text"
-        placeholder="Type your todo here"
-        className="input input-bordered w-full max-w-xs my-2 text-white"
-        value={currentTodo}
-        onChange={(e) => setCurrentTodo(e.target.value)}
-        // On Enter key down, create a new todoItem
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            addTodoItem();
-            setCurrentTodo("");
-          }
-        }}
-      ></input>
+      {email && (
+        <>
+          <label className="label">
+            <span className="label-text text-white">
+              What needs to get done?
+            </span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type your todo here"
+            className="input input-bordered w-full max-w-xs my-2 text-white"
+            value={currentTodo}
+            onChange={(e) => setCurrentTodo(e.target.value)}
+            // On Enter key down, create a new todoItem
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTodoItem();
+                setCurrentTodo("");
+              }
+            }}
+          ></input>
+        </>
+      )}
 
       {todos &&
         todos.map((todo) => (
@@ -134,6 +142,27 @@ function TodosWidget() {
           </div>
         ))}
       {todos.length > 0 && `${todos.length} items`}
+      {email === null && (
+        <div className="alert alert-error shadow-lg bg-red-700">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                color="white"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span className="text-white">Error! Unable to retrieve tasks.</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
