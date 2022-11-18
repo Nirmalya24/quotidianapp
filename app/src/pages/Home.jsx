625c540616e8ea28643e60fd60b03001168664f0
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-11-18 14:24:52 -0800

    Code cleanup

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-
-import Header from '../partials/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import Footer from '../partials/Footer';
+import React from "react";
+import Header from "../partials/Header";
+import HeroHome from "../partials/HeroHome";
+import FeaturesHome from "../partials/Features";
+import Footer from "../partials/Footer";
 
 function Home() {
   return (
